6f6df650da96e52a2580bd68fc249e73f47f1e04
V.Dara <76716485+Vireakdara@users.noreply.github.com>
2025-03-12 18:32:46 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -16,3 +16,50 @@ from utils import (
     non_max_suppression as nms,
     plot_image
 )
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+class YOLODataset(Dataset):
+    """Some Information about MyDataset"""
+    def __init__(
+        self, 
+        csv_file, 
+        img_dir, 
+        label_dir, 
+        anchors, 
+        image_size=416,
+        S=[13,26,52]
+        C=20,
+        transformer=None,
+    ):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.label_dir = label_dir
+        self.transformer = transformer
+        self.S = S
+        self.anchors = torch.tensor(anchors[0] + anchors[1] + anchors[2])  # for all 3 scales
+        self.num_anchors = self.anchors.shape[0]
+        self.num_anchors_per_scale = self.num_anchors // 3
+        self.C = C
+        self.ignore_iou_thresh = 0.5
+
+    def __getitem__(self, index):
+        label_path = os.path.join(self.label_dir,  self.annotations.iloc[index, 1])
+        bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), 4, axis=1).tolist()
+        img_path = os.path.join()
+        return 
+
+    def __len__(self):
+        return len(self.annotations)
+
+
+
+
+
+
+
+
+
+
+
+
