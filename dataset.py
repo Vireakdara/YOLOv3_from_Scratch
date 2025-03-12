7559b4f9749f9cf6d4ca442c032763e38c6b2549
V.Dara <76716485+Vireakdara@users.noreply.github.com>
2025-03-12 23:42:45 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,3 @@
-"""
-Dataset to load the Pascal VOC & MS COCO datasets
-"""
-
 import config
 import numpy as np
 import os
@@ -20,21 +16,21 @@ from utils import (
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class YOLODataset(Dataset):
-    """Some Information about MyDataset"""
     def __init__(
-        self, 
-        csv_file, 
-        img_dir, 
-        label_dir, 
-        anchors, 
+        self,
+        csv_file,
+        img_dir,
+        label_dir,
+        anchors,
         image_size=416,
-        S=[13,26,52]
+        S=[13, 26, 52],
         C=20,
         transform=None,
     ):
         self.annotations = pd.read_csv(csv_file)
         self.img_dir = img_dir
         self.label_dir = label_dir
+        self.image_size = image_size
         self.transform = transform
         self.S = S
         self.anchors = torch.tensor(anchors[0] + anchors[1] + anchors[2])  # for all 3 scales
@@ -43,18 +39,19 @@ class YOLODataset(Dataset):
         self.C = C
         self.ignore_iou_thresh = 0.5
 
+    def __len__(self):
+        return len(self.annotations)
+
     def __getitem__(self, index):
-        label_path = os.path.join(self.label_dir,  self.annotations.iloc[index, 1]) # Because 1 is the second column 
+        label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
         bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), 4, axis=1).tolist()
         img_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
         image = np.array(Image.open(img_path).convert("RGB"))
 
         if self.transform:
-            augmentations = self.transform(image=image , bboxes=bboxes)
-            image = augmentations[image]
-            bboxes = augmentations[bboxes]
-
-        targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S]
+            augmentations = self.transform(image=image, bboxes=bboxes)
+            image = augmentations["image"]
+            bboxes = augmentations["bboxes"]
 
         # Below assumes 3 scale predictions (as paper) and same num of anchors per scale
         targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S]
@@ -88,9 +85,42 @@ class YOLODataset(Dataset):
 
         return image, tuple(targets)
 
-    def __len__(self):
-        return len(self.annotations)
 
+def test():
+    anchors = config.ANCHORS
+
+    transform = config.test_transforms
+
+    dataset = YOLODataset(
+        "COCO/train.csv",
+        "COCO/images/images/",
+        "COCO/labels/labels_new/",
+        S=[13, 26, 52],
+        anchors=anchors,
+        transform=transform,
+    )
+    S = [13, 26, 52]
+    scaled_anchors = torch.tensor(anchors) / (
+        1 / torch.tensor(S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
+    )
+    loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
+    for x, y in loader:
+        boxes = []
+
+        for i in range(y[0].shape[1]):
+            anchor = scaled_anchors[i]
+            print(anchor.shape)
+            print(y[i].shape)
+            boxes += cells_to_bboxes(
+                y[i], is_preds=False, S=y[i].shape[2], anchors=anchor
+            )[0]
+        boxes = nms(boxes, iou_threshold=1, threshold=0.7, box_format="midpoint")
+        print(boxes)
+        plot_image(x[0].permute(1, 2, 0).to("cpu"), boxes)
+
+
+if __name__ == "__main__":
+    test()
 
 
 
